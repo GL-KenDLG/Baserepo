a043d4d604d0fad4c2ae1f311e93d8be3fa181bd
Ken De La Guera <kendelaguera@Kens-MacBook-Pro.local>
2017-12-07 14:34:54 -0500

    Tweaking the initial script

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -11,11 +11,6 @@ Write-Host "Installing Chrome"
 $client.DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', '$Home\Desktop\temp\chrome.exe')
 $Home\Desktop\temp\chrome.exe /silent /install;
 
-Write-Host "Installing Firefox"
-$client.DownloadFile('https://download-installer.cdn.mozilla.net/pub/firefox/releases/57.0.1/win64/en-US/Firefox%20Setup%2057.0.1.exe', '$Home\Desktop\temp\firefox.exe')
-@ECHO. @ECHO Installing Firefox @start /wait "$Home\Desktop\temp\firefox.exe" -ms
-
-
 Write-Host "Installing Notepad++"
 $client.DownloadFile('https://notepad-plus-plus.org/repository/7.x/7.3.3/npp.7.3.3.Installer.exe', '$Home\Desktop\temp\notepadplusplus.exe')
 $Home\Desktop\temp\notepadplusplus.exe /S
