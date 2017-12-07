b8e6fde0135c9c02a27fbd53faa6feb472f8bf81
Ken De La Guera <kendelaguera@Kens-MacBook-Pro.local>
2017-12-07 14:07:39 -0500

    Tweaking the initial script

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -1,28 +1,27 @@
 # Script to install a few basic programs in Windows
 
-@ECHO.
-
+Write-Host "Starting Windows Setup"
 $client = New-Object "System.Net.WebClient"
 
-@Echo Installing Chrome
+Write-Host "Installing Chrome"
 $client.DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', 'c:/temp/chrome.exe')
 c:/temp/chrome.exe /silent /install;
 
-@Echo Installing Firefox
+Write-Host "Installing Firefox"
 $client.DownloadFile('https://download-installer.cdn.mozilla.net/pub/firefox/releases/57.0.1/win64/en-US/Firefox%20Setup%2057.0.1.exe', 'c:/temp/firefox.exe')
 @start /wait "c:/temp/firefox.exe" -ms
 
 
-@Echo Installing Notepad plus plus
+Write-Host "Installing Notepad++"
 $client.DownloadFile('https://notepad-plus-plus.org/repository/7.x/7.3.3/npp.7.3.3.Installer.exe', 'c:/temp/notepadplusplus.exe')
 c:/notepadplusplus.exe /S
 
 
-@Echo Installing Slack
+Write-Host "Installing Slack"
 $client.DownloadFile('https://slack-ssb-updates.global.ssl.fastly.net/releases_x64/SlackSetup.msi', 'c:/temp/SlackSetup.msi')
 msiexec /i c:/temp/SlackSetup.msi /quiet /qn /norestart
 
-#Cleaning up
+Write-Host "Cleaning up"
 rm c:/temp -rec
 
 
