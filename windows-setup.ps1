8c1f9f59891b2f4f1c985da379738dbb29b6c943
Ken De La Guera <kendelaguera@Kens-MacBook-Pro.local>
2017-12-07 14:34:42 -0500

    Tweaking the initial script

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -2,30 +2,30 @@
 
 Write-Host "Creating temporary directory \temp"
 $fso = new-object -ComObject scripting.filesystemobject
-$fso.CreateFolder("C:\temp")
+$fso.CreateFolder("$Home\Desktop\temp")
 
 Write-Host "Starting Windows Setup"
 $client = New-Object "System.Net.WebClient"
 
 Write-Host "Installing Chrome"
-$client.DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', 'c:\temp\chrome.exe')
-c:\temp\chrome.exe /silent /install;
+$client.DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', '$Home\Desktop\temp\chrome.exe')
+$Home\Desktop\temp\chrome.exe /silent /install;
 
 Write-Host "Installing Firefox"
-$client.DownloadFile('https://download-installer.cdn.mozilla.net/pub/firefox/releases/57.0.1/win64/en-US/Firefox%20Setup%2057.0.1.exe', 'c:\temp\firefox.exe')
-@ECHO. @ECHO Installing Firefox @start /wait "c:\temp\firefox.exe" -ms
+$client.DownloadFile('https://download-installer.cdn.mozilla.net/pub/firefox/releases/57.0.1/win64/en-US/Firefox%20Setup%2057.0.1.exe', '$Home\Desktop\temp\firefox.exe')
+@ECHO. @ECHO Installing Firefox @start /wait "$Home\Desktop\temp\firefox.exe" -ms
 
 
 Write-Host "Installing Notepad++"
-$client.DownloadFile('https://notepad-plus-plus.org/repository/7.x/7.3.3/npp.7.3.3.Installer.exe', 'c:\temp\notepadplusplus.exe')
-c:\temp\notepadplusplus.exe /S
+$client.DownloadFile('https://notepad-plus-plus.org/repository/7.x/7.3.3/npp.7.3.3.Installer.exe', '$Home\Desktop\temp\notepadplusplus.exe')
+$Home\Desktop\temp\notepadplusplus.exe /S
 
 
 Write-Host "Installing Slack"
-$client.DownloadFile('https://slack-ssb-updates.global.ssl.fastly.net/releases_x64/SlackSetup.msi', 'c:\temp\SlackSetup.msi')
-msiexec /i c:\temp\SlackSetup.msi /quiet /qn /norestart
+$client.DownloadFile('https://slack-ssb-updates.global.ssl.fastly.net/releases_x64/SlackSetup.msi', '$Home\Desktop\temp\SlackSetup.msi')
+msiexec /i $Home\Desktop\temp\SlackSetup.msi /quiet /qn /norestart
 
 Write-Host "Cleaning up"
-rm c:\temp -rec
+rm $Home\Desktop\temp -rec
 
 
